7b8a3c68adcc6001efb967d7a7681bae76ee6f6f
Bsctuser <rohit@Rohits-MacBook-Air.local>
2023-02-05 18:11:55 +0530

    Updated Login

--- src/Main/Login.jsx
+++ src/Main/Login.jsx
@@ -1,11 +1,12 @@
 
-import { Card } from 'antd';
+import { Card, Divider } from 'antd';
 import { icons } from 'antd/es/image/PreviewGroup';
 const { Meta } = Card;
 import { Input, Space, Button, Checkbox, Form ,Alert} from 'antd';
 
 import ShareIcon from '../assets/shareicon.png';
 import { UserOutlined } from '@ant-design/icons'
+import Title from 'antd/es/skeleton/Title';
 
 
 const Login = () => (
@@ -13,18 +14,19 @@ const Login = () => (
     style={{
       width: 471,
       height: 570,
-      borderRadius: 40,
+      borderRadius: 30,
       background: '#FFFFFF',
-      borderColor: 'black'
+      borderColor:'black'
     }}
 
 
   >
     <img src={ShareIcon} style={{ width: 118, height: 92 }} />
-    <h2>LOGIN</h2>
+    <Title
+    <h3>Sign in to our account</h3>
     <Form >
-      <Space direction='vertical'>
-        <h3>Sign in to our account</h3>
+      
+        
         <Form.Item
           name="username"
           rules={[
@@ -35,7 +37,7 @@ const Login = () => (
 
           ]}
         >
-          <Input suffix={<UserOutlined />} placeholder="enter phone no or mail" size='large'/>
+          <Input placeholder='email or phone no' suffix={<UserOutlined/>} size='large'/>
         </Form.Item>
         <Form.Item
           name="password"
@@ -46,24 +48,27 @@ const Login = () => (
             },
           ]}
         >
-          <Input.Password placeholder='enter password' size='large'/>
+            <Input.Password placeholder='enter password' size='large'  />
+          
         </Form.Item>
         <Form.Item
           name="remember"
           valuePropName="checked"
           wrapperCol={{
             offset: 8,
-            span: 16,
+            span: 8,
           }}
         >
         <Checkbox >Remember me</Checkbox>
         </Form.Item>
-        <Button type='primary' htmlType='submit'>Login</Button>
+        <Button type='primary' htmlType='submit' style={{width:100}}>Login</Button>
+        <Divider style={{width:50}}/>
         <div>Not a member?<a>Sign Up now</a></div>
-      </Space>
+      
 
 
     </Form>
+    
   </Card>
 );
 export default Login;
