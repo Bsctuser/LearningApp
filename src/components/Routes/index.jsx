5659d1d885d01171cdaab496e8942c5be8ad11a2
rs-rohit44 <rohitshendkar25@gmail.com>
2023-02-06 16:57:39 +0530

    Home Page added

--- src/components/Routes/index.jsx
+++ src/components/Routes/index.jsx
@@ -1,12 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
 import Login from '../../Main/Login'
 import SignUp from '../../Main/SignUp'
-
+import Home from '../Home'
 const AppRouters = () => {
     return (
         <Routes>
             <Route path='/login' element={<Login/>}></Route>
             <Route path='/signup' element={<SignUp/>}/>
+            <Route path='/' element={<Home/>}/>
         </Routes>
     );
 
